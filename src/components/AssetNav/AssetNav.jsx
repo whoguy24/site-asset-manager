b6e8737d0489e0077e45afbd003f8dbd3c736fd2
whoguy24 <whoguy24@gmail.com>
2022-01-03 16:22:38 -0600

    Test commit

--- src/components/AssetNav/AssetNav.jsx
+++ src/components/AssetNav/AssetNav.jsx
@@ -5,65 +5,47 @@ function AssetNav() {
 
     const assets = useSelector(store => store.asset);
 
-    let buildings = [
-        {
-            name: 'Building A',
-            systems: [
-                {
-                    name:'Air Handling System',
-                    assets: [
-                        {name:'AHU-1'},
-                        {name:'AHU-2'},
-                        {name:'AHU-3'},
-                        {name:'AHU-4'},
-                        {name:'AHU-5'},
-                        {name:'AHU-6'}
-                    ]
-                },
-                {
-                    name:'Chilled Water System',
-                    assets: [
-                        {name:'CWP-1'},
-                        {name:'CWP-2'},
-                        {name:'CWP-3'},
-                    ]
-                },
-                {
-                    name:'Heating Hot Water System',
-                    assets: [
-                        {name:'HWP-1'},
-                        {name:'HWP-2'},
-                        {name:'HWP-3'},
-                    ]
-                }
-            ]
-        }
-    ]
-
-  // let uniqueBuildings = [... new Set(assets.map((asset) => asset.building))];
-
-
-
-
-
+    // Sample Data Structure- not actually used
+    // let buildings = [
+    //     {
+    //         name: 'Building A',
+    //         systems: [
+    //             {
+    //                 name:'Air Handling System',
+    //                 assets: [
+    //                     {name:'AHU-1'},
+    //                     {name:'AHU-2'},
+    //                     {name:'AHU-3'},
+    //                     {name:'AHU-4'},
+    //                     {name:'AHU-5'},
+    //                     {name:'AHU-6'}
+    //                 ]
+    //             },
+    //             {
+    //                 name:'Chilled Water System',
+    //                 assets: [
+    //                     {name:'CWP-1'},
+    //                     {name:'CWP-2'},
+    //                     {name:'CWP-3'},
+    //                 ]
+    //             },
+    //             {
+    //                 name:'Heating Hot Water System',
+    //                 assets: [
+    //                     {name:'HWP-1'},
+    //                     {name:'HWP-2'},
+    //                     {name:'HWP-3'},
+    //                 ]
+    //             }
+    //         ]
+    //     }
+    // ]
 
     const dispatch = useDispatch();
 
     useEffect(() => {
         dispatch({ type: 'FETCH_ASSETS' })
     }, [])
-
-    function buildBuildings() {
-
-        // 1. 
-
-    }
-
-    function buildSystems() {
-    }
-
-    function buildAssets() {
-    }
 
     return (
         <div>
